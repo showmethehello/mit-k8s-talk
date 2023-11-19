f894dc8011f464ba78a0c43dc89e556efc6299fe
Warren Hodgkinson <warren.hodgkinson@gmail.com>
2023-11-19 21:11:08 +0000

    Refactor myapp filenames

--- myapp-build-and-deploy.sh
+++ myapp-build-and-deploy.sh
@@ -5,10 +5,10 @@ TAG=$(date +%H%M%S)
 IMAGE=$REGISTRY/$APP:$TAG
 
 echo "Building $IMAGE"
-docker build . --build-arg VERSION=$TAG -f Dockerfile -t $IMAGE
+docker build . --build-arg VERSION=$TAG -f myapp-Dockerfile -t $IMAGE
 
 echo "Copying $IMAGE to kind cluster"
-kind load docker-image $IMAGE
+kind load docker-image $IMAGE --name mit
 
 echo "Applying deployment to kubenetes"
-cat "deployment.yaml" | sed "s/MYAPP_TAG/${TAG}/g" | kubectl apply -f -
+cat "myapp-deployment.yaml" | sed "s/MYAPP_TAG/${TAG}/g" | kubectl apply -f -
