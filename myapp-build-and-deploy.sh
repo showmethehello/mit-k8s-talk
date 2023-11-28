38d2fde6f87b67745b3e6f3410591defd83c1986
Warren Hodgkinson <warren.hodgkinson@gmail.com>
2023-11-28 09:48:17 +0000

    Tweaks

--- myapp-build-and-deploy.sh
+++ myapp-build-and-deploy.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
-REGISTRY=registry.mitmullingar.com
-APP=myapp
-TAG=$(date +%H%M%S)
+REGISTRY=wokstok #change this to your hub.docker username
+APP=mit-myapp
+TIME=$(date +%H%M%S)
+TAG=${1:-$TIME}
 IMAGE=$REGISTRY/$APP:$TAG
 
 echo "Building $IMAGE"
 docker build . --build-arg VERSION=$TAG -f myapp-Dockerfile -t $IMAGE
 
+# push the image from local docker to the cloud
+# registry. Optional if using "kind load docker-image" below
+# echo "Copying $IMAGE to cloud registry"
+# docker push $IMAGE
+
+# Use kind load docker-image to copy the image from docker to
+# your kind kubernetes cluster to save time downloading it
 echo "Copying $IMAGE to kind cluster"
 kind load docker-image $IMAGE --name mit
 
 echo "Applying deployment to kubenetes"
-cat "myapp-deployment.yaml" | sed "s/MYAPP_TAG/${TAG}/g" | kubectl apply -f -
+cat "myapp-deployment.yaml" | sed "s|MYAPP_IMAGE|${IMAGE}|g" | kubectl apply -f -
